5628fe9ea241e70eacd90a27811f42fe94b0d898
Fhmi00 <anisfahmi045@gmail.com>
2023-01-25 11:38:12 +0700

    backup

--- src/components/Header/home.js
+++ src/components/Header/home.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 import IconNotif from 'react-native-vector-icons/Feather';
+import Notification from '../../utils/notif';
 
 export default function HomeHeader(props) {
   const openDrawer = () => {
     props.navigation.openDrawer();
+  };
+  const handleNotification = () => {
+    console.log('Click Me !');
+    // [without schedule]
+    Notification.showNotification();
   };
   return (
     <View style={styles.header}>
@@ -13,7 +19,12 @@ export default function HomeHeader(props) {
         <Icon name="navicon" color="white" size={30} />
       </TouchableOpacity>
       <View>
-        <IconNotif name="message-square" color="white" size={30} />
+        <IconNotif
+          name="message-square"
+          color="white"
+          size={30}
+          onPress={handleNotification}
+        />
       </View>
     </View>
   );
